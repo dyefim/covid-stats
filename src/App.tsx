1b96b646bcaaba62e531ec798bdc47ea7df5ede5
dyefim <yefimchuk.dmytro@gmail.com>
2021-07-08 16:17:26 +0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">СOVID Stats</header>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Switch>
+            <Route exact path="/">
+              <div>Home</div>
+            </Route>
+            <Route path="/about">
+              <div>About</div>
+            </Route>
+            <Route path="/dashboard">
+              <div>Dashboard</div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
